2464d62070a550ed193c4c95f5546e072d063318
afloresep <afloresep01@gmail.com>
2025-02-19 15:20:23 +0100

    add setup for installing as project

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="three_desc",
+    version="0.1.0",
+    description="A package for computing 3D descriptors from molecular structures.",
+    author="Alejandro Flores",
+    packages=find_packages(),  # This will automatically find the "my_3d_descriptors" package folder
+    install_requires=[
+        "rdkit",
+        "numpy",
+        "pandas",
+        "matplotlib",
+    ],
+    extras_require={
+        "dev": [
+            "pytest",
+            "flake8",
+            "black",
+            "mypy",
+            "pytest-cov",
+        ],
+    },
+    python_requires=">=3.9",
+)
