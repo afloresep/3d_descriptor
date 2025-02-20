a4c931813af158e144bf1f18feadc33d30f12759
afloresep <afloresep01@gmail.com>
2025-02-20 09:56:09 +0100

    fix: descriptions to be docstring compatible

--- descriptors/a_method.py
+++ descriptors/a_method.py
@@ -4,7 +4,8 @@ from .base_descriptor import Base3DDescriptor
 from utils.chem_utils import _compute_MC2, _compute_fraction_sp3, _compute_fraction_carbon
 
 class ApproachA(Base3DDescriptor):
-    """ This is the first approach for the 3D descriptor. 
+    """This is the first approach for the 3D descriptor. 
+    
     Approach A:
     The 3D coordinates will be the following: 
       x = MC2 (custom complexity score). For more information on MC2 see compute_MC2() method. 
@@ -12,17 +13,20 @@ class ApproachA(Base3DDescriptor):
       z = fraction of carbon atoms
 
     Args:
-        Base3DDescriptor (_type_): _description_
+        Base3DDescriptor (Base Class): Base class for 3D descriptors.
     """
 
     def compute(self, mol) -> tuple[float, float, float]:
-        """_summary_
+        """Computes the 3D descriptor values for the given molecule.
 
         Args:
-            mol (_type_): _description_
+            mol (rdkit.Chem.mol): Molecular structure to compute descriptors for.
 
         Returns:
-            _type_: _description_
+            tuple[float, float, float]: A tuple containing:
+                - x: MC2 complexity score (float)
+                - y: Fraction of sp3-hybridized atoms (float)
+                - z: Fraction of carbon atoms (float)
         """
         x = _compute_MC2(mol)          # returns float
         y = _compute_fraction_sp3(mol) # returns float
